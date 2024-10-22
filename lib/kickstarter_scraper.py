2bb993179fa7c45516989f3aafd3fa4fb15515c9
Majak Deng <majakbanks.deng>
2024-10-22 14:58:49 +0300

    modified

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,31 +1,28 @@
 from bs4 import BeautifulSoup
-import ipdb
-
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
 def create_project_dict():
+    # Read the local HTML file
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+    
+    # Create BeautifulSoup object to parse HTML
     kickstarter = BeautifulSoup(html, 'html.parser')
+    
+    # Initialize projects dictionary
     projects = {}
-    # Iterate through the projects
+    
+    # Iterate through each project
     for project in kickstarter.select("li.project.grid_4"):
+        # Extract project title to use as dictionary key
         title = project.select("h2.bbcard_name strong a")[0].text
+        
+        # Create nested dictionary for project details
         projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+            'image_link': project.select("div.project-thumbnail a img")[0]['src'],
+            'description': project.select("p.bbcard_blurb")[0].text,
+            'location': project.select("ul.project-meta span.location-name")[0].text,
+            'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
         }
-    # return the projects dictionary
-
+    
     return projects
-
-projects = create_project_dict()
-print(projects)
